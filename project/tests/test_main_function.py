48500699882fd87a90bd0434f20c56e99f09d9c9
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-04-11 00:48:55 +0300

    version with edits

--- project/tests/test_main_function.py
+++ project/tests/test_main_function.py
@@ -1,8 +1,8 @@
 import datetime as dt
 import pytest
 
-
-from project.src import main
+from project.src.main import get_last_executed_operations, convert_date_strings
+from project.src.loader import load_operations
 
 
 @pytest.mark.parametrize(
@@ -16,11 +16,11 @@ from project.src import main
                     'state': 'EXECUTED',
                     'date': '2019-08-26T10:50:58.294041',
                     'operationAmount': {
-                      'amount': '31957.58',
-                      'currency': {
-                        'name': 'руб.',
-                        'code': 'RUB'
-                      }
+                        'amount': '31957.58',
+                        'currency': {
+                            'name': 'руб.',
+                            'code': 'RUB'
+                        }
                     },
                     'description': 'Перевод организации',
                     'from': 'Maestro 1596837868705199',
@@ -33,11 +33,11 @@ from project.src import main
                     'state': 'EXECUTED',
                     'date': dt.datetime(2019, 8, 26, 10, 50, 58, 294041),
                     'operationAmount': {
-                      'amount': '31957.58',
-                      'currency': {
-                        'name': 'руб.',
-                        'code': 'RUB'
-                      }
+                        'amount': '31957.58',
+                        'currency': {
+                            'name': 'руб.',
+                            'code': 'RUB'
+                        }
                     },
                     'description': 'Перевод организации',
                     'from': 'Maestro 1596837868705199',
@@ -84,7 +84,7 @@ from project.src import main
     ]
 )
 def test_convert_date_strings(operations, expected_data):
-    main.convert_date_strings(operations)
+    convert_date_strings(operations)
     assert operations == expected_data
 
 
@@ -92,7 +92,7 @@ def test_convert_date_strings(operations, expected_data):
     'json_filepath,expected_data',
     [
         pytest.param(
-            r'.\static\test_get_last_executed_operations\operations_without_date.json',
+            r'operations_without_date.json',
             [],
             id='operations_without_date'
         ),
@@ -100,5 +100,5 @@ def test_convert_date_strings(operations, expected_data):
 )
 def test_get_last_executed_operations(load_json_file, json_filepath, expected_data):
     operations = load_json_file(json_filepath)
-    last_executed_operations = main.get_last_executed_operations(operations)
+    last_executed_operations = get_last_executed_operations(operations)
     assert last_executed_operations == expected_data
