c86accebe8db5c8c7e9d58ef26a00d2c0fac271d
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-04-11 00:48:55 +0300

    version with edits

--- project/src/loader.py
+++ project/src/loader.py
@@ -1,9 +1,13 @@
 import requests
 import json
 
+"""
+Функция load_operations, для открытия файла operations.json
+"""
+
 
 def load_operations():
-    with open('operations.json', 'r', encoding='utf-8') as file:
+    with open('../operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
